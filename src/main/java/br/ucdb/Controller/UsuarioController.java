9767c0bc575a9d43ce3478c134f4bb9eed92758c
Hudson Machado <Hudsonmachado2012@gmail.com>
2016-11-10 19:52:06 -0300

    Implementado o CRUD Usuario

---
+++ src/main/java/br/ucdb/Controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package br.ucdb.Controller;
+
+import br.ucdb.Repository.UsuarioRepository;
+import br.ucdb.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @PostMapping("/usuario")
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        return new ResponseEntity<Usuario>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/usuario")
+    public List<Usuario> buscarUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    @PutMapping("/usuario")
+    public ResponseEntity<Usuario> atualizaCadastro(Usuario usuario){
+            return new ResponseEntity<Usuario>(usuarioRepository.save(usuario), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/usuario")
+    public void excluirUsuario(@RequestBody Usuario usuario){
+
+            usuarioRepository.delete(usuario);
+
+    }
+
+}
--- src/main/java/br/ucdb/Repository/UsuarioRepository.java
+++ src/main/java/br/ucdb/Repository/UsuarioRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     @Query(value = "Select u from Usuario u where u.login=:userLogin")
     public Usuario buscaPorId(@Param("userLogin") String login);
+
+    @Query("select u from Usuario u order by u.login")
+    public List<Usuario> buscarUsuarios();
 }
