a2486a066e580604cbe2cb3c9ff7958d66e3cc87
Hudson Machado <Hudsonmachado2012@gmail.com>
2016-11-23 21:55:20 -0300

    Implementado pelo grupo Cadastro e Listagem de Usuario

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -17,6 +17,6 @@ dicionarioApp.config(function ($routeProvider,$locationProvider ) {
         })
         .otherwise({redirectTo:'/'});
 
-    $locationProvider.html5Mode(true);
+   // $locationProvider.html5Mode(true);
 
 });
