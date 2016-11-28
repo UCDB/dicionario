fadb63ec9ec4904bdc9401bc580f7618c4ad42e1
Hudson Machado <Hudsonmachado2012@gmail.com>
2016-11-28 19:24:54 -0300

    Atualizado funcionalidade de upload de imagem

--- src/main/resources/static/js/controller/paginaPrincipal-controller.js
+++ src/main/resources/static/js/controller/paginaPrincipal-controller.js
@@ -1,6 +1,8 @@
-dicionarioApp.controller("paginaInicial-controller",['$scope', '$location', function ($scope, $location) {
+dicionarioApp.controller("paginaInicial-controller",['$scope', '$location','$http', function ($scope, $location,$http) {
 
-
+    $(document).ready(function(){
+        carregaPalavras();
+    });
 
 // Função Busca Palavra Jquery
     $(function(){
@@ -30,16 +32,96 @@ dicionarioApp.controller("paginaInicial-controller",['$scope', '$location', func
         });
     });
 
+    var objPalavra = {};
 
-// Função Edit Jquery
-//var _trEdit = null;
-//$(document).on('click', '.editRowButton', function(){
-//_trEdit = $(this).closest('tr');
-//$(_trEdit).find('td:eq(0)').text();
+    function carregaPalavras(){
 
-//$('input[name="id"]').val(_id);
+        $http.get("/dicionario").then(
+            function (response) {
+                objPalavras = response.data;
+                //alert('Carregado !');
+            },function (response) {
+                alert('Erro ao buscar palavras !');
+            });
 
-//});
+
+        /*$.get("palavras", function(data,status){
+
+         var objPalavras = JSON.parse(data);
+
+         for(i=0; i< objPalavra.length; i++) {
+         //valores = '<td>'+ objPalavras[i].id +'</td>';
+         }
+         }
+
+
+         $('.corpoTabela').append(valores);
+
+         });*/
+    }
+
+    function abrirPalavra(idPalavra){
+
+        // AJAX busca dados da palavra
+        // chamar funcao que preenche objeto JSON com todos os dados da palavra
+
+        objPalavra = {
+            id: 1,
+            titulo: 'titulo',
+            descricao: 'descricao',
+            data: '22/11/2016'
+        };
+
+        $('#idPalavra').val( objPalavra.id );
+        $('#tituloPalavra').val( objPalavra.titulo );
+        $('#descricaoPalavra').val( objPalavra.descricao );
+        $('#dataPalavra').val( objPalavra.data );
+
+        $('#visualizaPalavra').html('aberta a palavra: '+idPalavra);
+    }
+
+    function commitPalavra(){
+
+        var tipo;
+        var acao;
+        if($('#idPalavra').val() == ''){
+            tipo = 'POST';
+            acao = 'inserida';
+        }else{
+            tipo = 'PUT';
+            acao = 'alterada';
+        }
+
+        // JEITO 1
+        objPalavra.id = $('#idPalavra').val();
+        objPalavra.titulo = $('#tituloPalavra').val();
+        objPalavra.descricao = $('#descricaoPalavra').val();
+        objPalavra.data = $('#dataPalavra').val();
+
+        /* JEITO 2
+         objPalavra = {
+         id: $('#idPalavra').val(),
+         titulo: $('#tituloPalavra').val(),
+         descricao: $('#descricaoPalavra').val(),
+         data: $('#dataPalavra').val()
+         };*/
+
+        // AJAX  INSERT AND UPDATE
+        $.ajax({
+            url:'dicionario',
+            type:tipo,
+            dataType:'json',
+            contentType:'application/json;charset=UTF-8',
+            data:JSON.stringify(objPalavra),
+            success:function(){
+                window.alert('Palavra '+ acao +' com Sucesso!');
+                carregaPalavras();
+            }, error:function(){
+                alert('Erro ao tentar executar!');
+            }
+        });
+
+    }
 
 
 }]);
